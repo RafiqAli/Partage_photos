3cc555e9a64ff2475fca955de06a8a7f6e642aec
RafiqAli <ali.rafiq.131@gmail.com>
2017-03-17 02:13:44 +0000

    added random string generator function

--- core/Helpers.php
+++ core/Helpers.php
@@ -26,6 +26,22 @@ class Helpers {
 	}
 
 
+    public static function RandomString($length = 8) {
+
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        $charactersLength = strlen($characters);
+
+        $randomString = '';
+        
+        for ($i = 0; $i < $length; $i++) {
+        
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        
+        return $randomString;
+    }
+
 
     /**
      * This function displays a table fields and contents
@@ -133,6 +149,7 @@ class Helpers {
     }
 
 }
+
 
 
 ?>
