9c4b76d9ee15dda7514c1495c1717b2ccfe549bc
Yassir Khaldi <khaldi.yass@gmail.com>
2017-03-19 18:58:31 +0000

    Project Routes updated for controllers

--- routes.php
+++ routes.php
@@ -1,15 +1,25 @@
 <?php
-  function call($controller, $action) {
+
+  function call($controller, $action) 
+  {
     require_once('controllers/' . $controller . '_controller.php');
 
     switch($controller) {
+     
       case 'pages':
+        if($action=='login' || $action=='register')
+        {
+            require_once('Models/User.php');
+
+        }
         $controller = new PagesController();
       break;
-      case 'posts':
+
+      case 'photos':
         // we need the model to query the database later in the controller
-        require_once('models/post.php');
-        $controller = new PostsController();
+        require_once('Models/Photo.php');
+        require_once('Models/User.php');
+        $controller = new PhotosController();
       break;
     }
 
@@ -17,8 +27,9 @@
   }
 
   // we're adding an entry for the new controller and its actions
-  $controllers = array('pages' => ['home', 'error'],
-                       'posts' => ['index', 'show']);
+  $controllers = array('pages' => ['home', 'login', 'register', 'logout', 'error'],
+                        'photos' => ['show_all', 'mes_photos', 'affiche_photo', 'modif_photo']
+                       );
 
   if (array_key_exists($controller, $controllers))
   {
