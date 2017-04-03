dfc400425d84c5dbba28003c49e8a2385ad55d4a
RafiqAli <ali.rafiq.131@gmail.com>
2017-04-03 12:07:54 +0100

    added club and its relationships to photo and user

--- database.sql
+++ database.sql
@@ -2,10 +2,10 @@
 -- version 4.6.6
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 18, 2017 at 10:21 PM
--- Server version: 5.7.17-0ubuntu0.16.04.1
--- PHP Version: 7.0.15-0ubuntu0.16.04.4
+-- Client :  localhost
+-- Généré le :  Lun 03 Avril 2017 à 11:59
+-- Version du serveur :  5.7.17-0ubuntu0.16.04.1
+-- Version de PHP :  7.0.15-0ubuntu0.16.04.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,34 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `partage_photos`
+-- Base de données :  `partage_photos`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comments`
+-- Structure de la table `clubs`
+--
+
+CREATE TABLE `clubs` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `admin` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `clubs`
+--
+
+INSERT INTO `clubs` (`id`, `title`, `description`, `admin`) VALUES
+(1, 'friends', 'this group is made for friends to meet and get to know each other more and share what they cherish with each other.', 'ali'),
+(2, 'lovers', 'where lovers meet.', 'yassir');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comments`
 --
 
 CREATE TABLE `comments` (
@@ -34,7 +55,7 @@ CREATE TABLE `comments` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `comments`
+-- Contenu de la table `comments`
 --
 
 INSERT INTO `comments` (`id`, `content`, `owner`, `photo_id`) VALUES
@@ -47,7 +68,7 @@ INSERT INTO `comments` (`id`, `content`, `owner`, `photo_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `photos`
+-- Structure de la table `photos`
 --
 
 CREATE TABLE `photos` (
@@ -57,23 +78,45 @@ CREATE TABLE `photos` (
   `date` date NOT NULL,
   `description` text NOT NULL,
   `file` varchar(255) NOT NULL,
-  `owner` varchar(255) NOT NULL
+  `owner` varchar(255) NOT NULL,
+  `visibility` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `photos`
+-- Contenu de la table `photos`
 --
 
-INSERT INTO `photos` (`id`, `title`, `name`, `date`, `description`, `file`, `owner`) VALUES
-(18, 'PhotoTest', '14212165_1379492905441163_7402284545959690173_n.jpg', '2017-03-15', 'this is a description', 'mDEAbMBr.jpeg', 'ali'),
-(19, 'PhotoTest', '14212165_1379492905441163_7402284545959690173_n.jpg', '2017-03-15', 'this is a description', 'GX3l4fZI.jpeg', 'ali'),
-(21, 'this is a name', 'tightropewalkers.jpg', '2017-03-15', 'this is a description', 'iC3NPOEb.jpeg', 'ali'),
-(24, 'oooooooooooooo', '12767598_1682830325319227_1382226467_n.jpg', '2017-03-21', 'ooooooooooooooooooo', 'sOpEIUL1.jpeg', 'yassir');
+INSERT INTO `photos` (`id`, `title`, `name`, `date`, `description`, `file`, `owner`, `visibility`) VALUES
+(18, 'PhotoTest', '14212165_1379492905441163_7402284545959690173_n.jpg', '2017-03-15', 'this is a description', 'mDEAbMBr.jpeg', 'ali', 0),
+(19, 'PhotoTest', '14212165_1379492905441163_7402284545959690173_n.jpg', '2017-03-15', 'this is a description', 'GX3l4fZI.jpeg', 'ali', 0),
+(21, 'this is a name', 'tightropewalkers.jpg', '2017-03-15', 'this is a description', 'iC3NPOEb.jpeg', 'ali', 0),
+(24, 'oooooooooooooo', '12767598_1682830325319227_1382226467_n.jpg', '2017-03-21', 'ooooooooooooooooooo', 'sOpEIUL1.jpeg', 'yassir', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `photo_club`
+--
+
+CREATE TABLE `photo_club` (
+  `photo_id` int(11) NOT NULL,
+  `club_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `photo_club`
+--
+
+INSERT INTO `photo_club` (`photo_id`, `club_id`) VALUES
+(18, 1),
+(19, 1),
+(21, 2),
+(24, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
 --
 
 CREATE TABLE `users` (
@@ -82,47 +125,81 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Contenu de la table `users`
 --
 
 INSERT INTO `users` (`login`, `password`) VALUES
 ('ali', 'e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4'),
 ('yassir', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8');
 
+-- --------------------------------------------------------
+
 --
--- Indexes for dumped tables
+-- Structure de la table `user_club`
+--
+
+CREATE TABLE `user_club` (
+  `user_login` varchar(255) NOT NULL,
+  `club_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
 --
 
 --
--- Indexes for table `comments`
+-- Index pour la table `clubs`
+--
+ALTER TABLE `clubs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `comments`
 --
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `photos`
+-- Index pour la table `photos`
 --
 ALTER TABLE `photos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Index pour la table `photo_club`
+--
+ALTER TABLE `photo_club`
+  ADD PRIMARY KEY (`photo_id`,`club_id`);
+
+--
+-- Index pour la table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`login`),
   ADD UNIQUE KEY `login` (`login`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Index pour la table `user_club`
+--
+ALTER TABLE `user_club`
+  ADD PRIMARY KEY (`user_login`,`club_id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT for table `comments`
+-- AUTO_INCREMENT pour la table `clubs`
+--
+ALTER TABLE `clubs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
--- AUTO_INCREMENT for table `photos`
+-- AUTO_INCREMENT pour la table `photos`
 --
 ALTER TABLE `photos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
