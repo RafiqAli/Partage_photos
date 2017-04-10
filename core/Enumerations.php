4550d7b6e815e8eab8adb98d59d8235ebbc55b9d
RafiqAli <ali.rafiq.131@gmail.com>
2017-04-10 13:30:02 +0100

    Arranged all contant variable used as enumeration in one file.

---
+++ core/Enumerations.php
@@ -0,0 +1,41 @@
+<?php
+
+
+class Sort
+{
+	const NEWEST    = 0;
+
+	const OLDEST    = 1;
+
+	const TOP_RATED = 2;
+
+	const LOW_RATED = 3;
+}
+
+class Search
+{
+	const TEXT             = 0;
+
+	const OWNER            = 1;
+
+	const BEFORE_DATE      = 2;
+
+	const AFTER_DATE       = 3;
+
+	const DATE             = 4;
+
+	const BETWEEN_DATE     = 5;
+
+}
+
+class Visibility
+{
+	const OPEN   = 1;
+
+	const CLOSED = 2;
+	
+	const SHARED = 3;
+}
+
+
+?>
