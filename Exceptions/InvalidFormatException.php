271b53828540584f1177cba7fc463b68983d34a6
RafiqAli <ali.rafiq.131@gmail.com>
2017-04-18 19:50:29 +0100

    updated invalidformatexception

--- Exceptions/InvalidFormatException.php
+++ Exceptions/InvalidFormatException.php
@@ -1,12 +1,12 @@
 <?php
 
 
-class customException extends Exception {
+class InvalidFormatException extends Exception {
 
   public function errorMessage() {
 
     //error message
-    $errorMsg = 'Error on line '.$this->getLine().' in '.$this->getFile() .': <b>'.$this->getMessage().'</b> is not a valid E-Mail address';
+    $errorMsg = 'Error on line '.$this->getLine().' in '.$this->getFile() .': <b>'.$this->getMessage().'</b> the format of the argument is invalid';
     
     return $errorMsg;
   }
